d6335d5d1299ed433e47c2d2248709901a50fa43
euimokna <euimok@gmail.com>
2022-09-16 13:19:47 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,6 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/DevOpsProduct/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket_prefix = "s3edu"
-  #prefix = "euimok"
+  bucket_prefix = "euimok"
 }
